8a6276db96fedd84fa7b9146e4a27962339d62c4
guox <guox@goodrain.com>
2019-06-22 22:54:36 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,8 @@ if [[ ${SERVICE_POD_NUM} -gt 1 ]];then
     for i in $(seq ${SERVICE_POD_NUM})
       do
         echo "discover the member of rabbit cluster : rabbit@${HOSTNAME%-*}-`expr $i - 1`.${HOSTNAME%-*}.${TENANT_ID}.svc.cluster.local."
-        echo "cluster_formation.classic_config.nodes.$i = rabbit@${HOSTNAME%-*}-`expr $i - 1`.${HOSTNAME%-*}.${TENANT_ID}.svc.cluster.local." >> /opt/rabbitmq/etc/rabbitmq/rabbit.conf
+        #echo "cluster_formation.classic_config.nodes.$i = rabbit@${HOSTNAME%-*}-`expr $i - 1`.${HOSTNAME%-*}.${TENANT_ID}.svc.cluster.local." >> /opt/rabbitmq/etc/rabbitmq/rabbit.conf
+        echo "cluster_formation.classic_config.nodes.$i = rabbit@${HOSTNAME%-*}-`expr $i - 1`.${HOSTNAME%-*}" >> /opt/rabbitmq/etc/rabbitmq/rabbit.conf
       done
 fi
 
